33da2ccebaf57b619e009d01f9c8b50be3eaa090
SK ABDUL FAHEMID <53863051+FAHEMID03@users.noreply.github.com>
2026-07-14 12:09:50 +0530

    Convert TC_04_Overflow to overflow_seq class
    Refactor TC_04_Overflow test case into overflow_seq class.

--- UVM_TESTBENCH/TC_04_Overflow.sv
+++ UVM_TESTBENCH/TC_04_Overflow.sv
@@ -1,13 +1,21 @@
-task TC_04_Overflow;
+class overflow_seq extends uvm_sequence #(adder_seq_item);
 
-begin
+   adder_seq_item req;
 
-    $display("Running TC_04");
+   `uvm_object_utils(overflow_seq)
 
-    send_transaction(32'hFFFFFFFF,32'h1);
+   task body();
 
-    check_result(32'h00000000);
+      req=adder_seq_item::type_id::create("req");
 
-end
+      start_item(req);
 
-endtask
+      req.A=32'hFFFFFFFF;
+      req.B=32'h1;
+      req.Start_i=1;
+
+      finish_item(req);
+
+   endtask
+
+endclass

cd2ccf6d48bbe3cc4fd709aa4c4672876eac05c6
SK ABDUL FAHEMID <53863051+FAHEMID03@users.noreply.github.com>
2026-07-14 22:11:39 +0530

    Add Active_passive_agent.sv for active/passive configuration

---
+++ UVM_TESTBENCH/AGENT/Active_passive_agent.sv
@@ -0,0 +1,19 @@
+uvm_config_db#(uvm_active_passive_enum)::set
+
+(
+   null,
+   "*",
+   "is_active",
+   UVM_ACTIVE
+);
+
+
+// Or//
+uvm_config_db#(uvm_active_passive_enum)::set
+
+(
+   null,
+   "*",
+   "is_active",
+   UVM_PASSIVE
+);

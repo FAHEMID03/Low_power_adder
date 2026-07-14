7f1c4dcc95925e0dee3141596bfc7659d88e8745
SK ABDUL FAHEMID <53863051+FAHEMID03@users.noreply.github.com>
2026-07-14 17:56:29 +0530

    Add adder_sequencer class to sequencer.sv

---
+++ UVM_TESTBENCH/SEQUENCER/sequencer.sv
@@ -0,0 +1,12 @@
+class adder_sequencer extends uvm_sequencer #(adder_seq_item);
+
+   `uvm_component_utils(adder_sequencer)
+
+   function new(string name,
+                uvm_component parent);
+
+      super.new(name,parent);
+
+   endfunction
+
+endclass

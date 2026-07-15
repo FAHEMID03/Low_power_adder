6bc489294183435e9fe3068e2c8bfeceda4cdf84
SK ABDUL FAHEMID <53863051+FAHEMID03@users.noreply.github.com>
2026-07-15 17:37:23 +0530

    Update TC_05_Random_Addition.sv

--- UVM_TESTBENCH/SEQUENCE/TC_05_Random_Addition.sv
+++ UVM_TESTBENCH/SEQUENCE/TC_05_Random_Addition.sv
@@ -1,4 +1,4 @@
-class random_seq extends uvm_sequence #(adder_seq_item);
+/*class random_seq extends uvm_sequence #(adder_seq_item);
 
    adder_seq_item req;
 
@@ -19,6 +19,50 @@ class random_seq extends uvm_sequence #(adder_seq_item);
          req.Start_i=1;
 
          finish_item(req);
+
+      end
+
+   endtask
+
+endclass*/
+
+class random_add_seq extends adder_base_seq;
+
+   `uvm_object_utils(random_add_seq)
+
+   bit [31:0] a;
+   bit [31:0] b;
+
+   bit [31:0] result;
+   bit [31:0] status;
+
+   task body();
+
+      repeat(100)
+
+      begin
+
+         a=$urandom();
+
+         b=$urandom();
+
+         axi_write('h0000,a);
+
+         axi_write('h0004,b);
+
+         axi_write('h0008,1);
+
+         do
+
+            axi_read('h000C,status);
+
+         while(status[0]==0);
+
+         axi_read('h0010,result);
+
+         if(result!=(a+b))
+
+            `uvm_error("SEQ","Random Failed");
 
       end
 

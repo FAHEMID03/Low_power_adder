7ae52aba3a983c3be1c515321437cd6649161931
SK ABDUL FAHEMID <53863051+FAHEMID03@users.noreply.github.com>
2026-07-15 17:36:40 +0530

    Refactor overflow_seq class for adder_base_seq

--- UVM_TESTBENCH/SEQUENCE/TC_04_Overflow.sv
+++ UVM_TESTBENCH/SEQUENCE/TC_04_Overflow.sv
@@ -1,4 +1,4 @@
-class overflow_seq extends uvm_sequence #(adder_seq_item);
+/*class overflow_seq extends uvm_sequence #(adder_seq_item);
 
    adder_seq_item req;
 
@@ -15,6 +15,36 @@ class overflow_seq extends uvm_sequence #(adder_seq_item);
       req.Start_i=1;
 
       finish_item(req);
+
+   endtask
+
+endclass*/
+class overflow_seq extends adder_base_seq;
+
+   `uvm_object_utils(overflow_seq)
+
+   bit [31:0] status;
+   bit [31:0] result;
+
+   task body();
+
+      axi_write('h0000,32'hFFFFFFFF);
+
+      axi_write('h0004,32'h1);
+
+      axi_write('h0008,1);
+
+      do
+
+         axi_read('h000C,status);
+
+      while(status[0]==0);
+
+      axi_read('h0010,result);
+
+      if(result!=32'h0)
+
+         `uvm_error("SEQ","Overflow Failed");
 
    endtask
 

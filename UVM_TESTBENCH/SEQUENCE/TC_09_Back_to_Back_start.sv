85590c7393e7764acdc6f0abfa29205e3521839a
SK ABDUL FAHEMID <53863051+FAHEMID03@users.noreply.github.com>
2026-07-15 17:38:50 +0530

    Update TC_09_Back_to_Back_start.sv

--- UVM_TESTBENCH/SEQUENCE/TC_09_Back_to_Back_start.sv
+++ UVM_TESTBENCH/SEQUENCE/TC_09_Back_to_Back_start.sv
@@ -1,4 +1,4 @@
-class back_to_back_seq extends uvm_sequence #(adder_seq_item);
+/*class back_to_back_seq extends uvm_sequence #(adder_seq_item);
 
    adder_seq_item req;
 
@@ -23,6 +23,28 @@ class back_to_back_seq extends uvm_sequence #(adder_seq_item);
          // No idle cycle
 
       end
+
+   endtask
+
+endclass*/
+
+class back_to_back_seq extends adder_base_seq;
+
+   `uvm_object_utils(back_to_back_seq)
+
+   task body();
+
+      axi_write('h0000,10);
+
+      axi_write('h0004,20);
+
+      axi_write('h0008,1);
+
+      axi_write('h0000,30);
+
+      axi_write('h0004,40);
+
+      axi_write('h0008,1);
 
    endtask
 

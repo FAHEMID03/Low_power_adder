bf0ccd13be1da7e29ee043b8c48bd869932aed9b
SK ABDUL FAHEMID <53863051+FAHEMID03@users.noreply.github.com>
2026-07-15 17:35:15 +0530

    Refactor basic_add_seq for addition sequence
    Refactor basic_add_seq class to implement new structure and add functionality for operand writing and result reading.

--- UVM_TESTBENCH/SEQUENCE/TC_02_Basic_addtion.sv
+++ UVM_TESTBENCH/SEQUENCE/TC_02_Basic_addtion.sv
@@ -1,6 +1,6 @@
-class basic_add_seq extends uvm_sequence #(adder_seq_item);
+class basic_add_seq extends Adder_base_seq //uvm_sequence #(adder_seq_item);
 
-   adder_seq_item req;
+  /* adder_seq_item req;
 
    `uvm_object_utils(basic_add_seq)
 
@@ -19,6 +19,62 @@ class basic_add_seq extends uvm_sequence #(adder_seq_item);
       req.Start_i=1;
 
       finish_item(req);
+
+   endtask
+
+endclass*/
+
+   `uvm_object_utils(basic_add_seq)
+
+   bit [31:0] result;
+   bit [31:0] status;
+
+   task body();
+
+      //----------------------------------------
+      // Write Operand A
+      //----------------------------------------
+
+      axi_write(32'h0000,32'd5);
+
+      //----------------------------------------
+      // Write Operand B
+      //----------------------------------------
+
+      axi_write(32'h0004,32'd3);
+
+      //----------------------------------------
+      // Start
+      //----------------------------------------
+
+      axi_write(32'h0008,32'h1);
+
+      //----------------------------------------
+      // Wait Data Valid
+      //----------------------------------------
+
+      do
+      begin
+
+         axi_read(32'h000C,status);
+
+      end
+
+      while(status[0]==0);
+
+      //----------------------------------------
+      // Read Result
+      //----------------------------------------
+
+      axi_read(32'h0010,result);
+
+      if(result==8)
+
+         `uvm_info("SEQ","PASS",UVM_LOW)
+
+      else
+
+         `uvm_error("SEQ","FAIL")
 
    endtask
 
